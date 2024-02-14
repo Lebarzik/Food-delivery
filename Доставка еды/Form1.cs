2f1ff3a7889e6c879e3191ac98c09a708b7d2efc
Lebarzik <156694507+Lebarzik@users.noreply.github.com>
2024-02-14 19:37:18 +0300

    Add files via upload

---
+++ Доставка еды/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Доставка_еды
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*GraphicsPath path = new GraphicsPath();
+
+            Rectangle rect = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            int diameter = 10 * 2;
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddArc(rect.Right - diameter, rect.Top, diameter, diameter, 270, 90);
+            gp.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            gp.AddArc(rect.Left, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            gp.AddArc(rect.Left, rect.Top, diameter, diameter, 180, 90);
+
+            gp.CloseFigure();
+
+            Region region = new Region(gp);
+            pictureBox1.Region = region;*/
+
+            Border_Radius(pictureBox1, 10);
+            Border_Radius(pictureBox2, 10);
+        }
+
+        private void Border_Radius(PictureBox img, int radius)
+        {
+            Rectangle rect = new Rectangle(0, 0, img.Width, img.Height);
+            GraphicsPath path = new GraphicsPath();
+
+            int diameter = radius * 2;
+            GraphicsPath gp = new GraphicsPath();
+
+            gp.AddArc(rect.Right - diameter, rect.Top, diameter, diameter, 270, 90);
+            gp.AddArc(rect.Right - diameter, rect.Bottom - diameter, diameter, diameter, 0, 90);
+            gp.AddArc(rect.Left, rect.Bottom - diameter, diameter, diameter, 90, 90);
+            gp.AddArc(rect.Left, rect.Top, diameter, diameter, 180, 90);
+
+            gp.CloseFigure();
+
+            Region region = new Region(gp);
+            img.Region = region;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
